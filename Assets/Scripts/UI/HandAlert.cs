089a1ee7eb6d2443027a55d055632d37f2224619
Thanawit Prasongpongchai <taepras@gmail.com>
2016-05-15 23:20:45 +0700

    Add "HAND NOT DETECTED" label to each scenes

---
+++ Assets/Scripts/UI/HandAlert.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+
+public class HandAlert : MonoBehaviour {
+
+	LeapProvider provider;
+
+	private CanvasRenderer cr;
+	// Use this for initialization
+	void Start () {
+		provider = FindObjectOfType<LeapProvider> () as LeapProvider;	
+		cr = GetComponent<CanvasRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (provider.CurrentFrame.Hands.Count == 0){
+			cr.SetAlpha (1f);
+			for (int i = 0; i < transform.childCount; i++) {
+				transform.GetChild (i).GetComponent<CanvasRenderer> ().SetAlpha (1f);
+			}
+		}else{
+			cr.SetAlpha (0f);
+			for (int i = 0; i < transform.childCount; i++) {
+				transform.GetChild (i).GetComponent<CanvasRenderer> ().SetAlpha (0f);
+			}
+		}
+	}
+}
